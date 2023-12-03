ce64fbe78f7830967aaa5749a13e4d299730d742
geordie <geordie.a.henderson@gmail.com>
2023-12-03 12:34:20 -0800

    Solution for day 3, puzzle 1

--- main.go
+++ main.go
@@ -3,24 +3,29 @@ package main
 import (
 	day1 "github.com/geordie/adventofcode2023/day1"
 	day2 "github.com/geordie/adventofcode2023/day2"
+	day3 "github.com/geordie/adventofcode2023/day3"
 )
 
 func main() {
-	day2puzzle2()
+	day3puzzle1()
 }
 
-func day1puzzle1() {
-	day1.Day1Puzzle1()
+func day3puzzle1() {
+	day3.Day3Puzzle1()
 }
 
 func day2puzzle1() {
 	day2.Day2Puzzle1()
 }
 
-func day1puzzle2() {
-	day1.Day1Puzzle2()
-}
-
 func day2puzzle2() {
 	day2.Day2Puzzle2()
+}
+
+func day1puzzle1() {
+	day1.Day1Puzzle1()
+}
+
+func day1puzzle2() {
+	day1.Day1Puzzle2()
 }
